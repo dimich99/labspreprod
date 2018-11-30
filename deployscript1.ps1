bef131e67c80c9a7aad8b2288f7d3c4211bc7dcf
Dima Gladunets <40930080+dimich99@users.noreply.github.com>
2018-11-30 09:02:30 +0200

    Update deployscript1.ps1

--- deployscript1.ps1
+++ deployscript1.ps1
@@ -1,4 +1,4 @@
-Set-NetFirewallProfile –Enabled False
+Set-NetFirewallProfile -Enabled False
 Add-WindowsFeature Web-Server
 Add-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value HelloWorld1
 Set-WebBinding -Name 'Default Web Site' -BindingInformation *:80: -PropertyName Port -Value 8080
